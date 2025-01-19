2e64cb79b224669156b7183bb0c31c545aa1270a
FSemti Dev <fabrice@fabricesemti.com>
2025-01-19 18:48:17 +0000

    refactor: :zap: rename swarm hosts
    to allign with the docker swarm playbook

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   containers = {
-    "container0" = {
+    "swarm-m0" = {
 
       target_node = "pve-0"
-      hostname    = "docker-swarmhost-0"
+      hostname    = "swarm-m0"
       ostemplate  = "nfs-qnap-proxmox:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
       storage     = "cp"
       size        = "16G"
@@ -14,31 +14,31 @@ locals {
         gw     = "10.0.40.1"
       }
     },
-    "container1" = {
+    "swarm-w0" = {
 
       target_node = "pve-1"
-      hostname    = "docker-swarmhost-1"
+      hostname    = "swarm-w0"
       ostemplate  = "nfs-qnap-proxmox:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
       storage     = "local-lvm"
       size        = "16G"
       network = {
         name   = "eth0"
         bridge = "vmbr0"
-        ip     = "10.0.40.111/24"
+        ip     = "10.0.40.120/24"
         gw     = "10.0.40.1"
       }
     },
-    "container2" = {
+    "swarm-w1" = {
 
       target_node = "pve-0"
-      hostname    = "docker-swarmhost-2"
+      hostname    = "swarm-w0"
       ostemplate  = "nfs-qnap-proxmox:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
       storage     = "cp"
       size        = "16G"
       network = {
         name   = "eth0"
         bridge = "vmbr0"
-        ip     = "10.0.40.112/24"
+        ip     = "10.0.40.121/24"
         gw     = "10.0.40.1"
       }
     }
